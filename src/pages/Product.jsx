97a0f46c7e002b724b8b316e485c26b9cef541d7
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-10-30 14:33:54 +0100

    .

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export const Product = () => {
-  return <div>Product</div>;
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:8081/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.error(error));
+  }, [id]);
+  console.log(product);
+
+  return (
+    <div>
+      {product && (
+        <>
+          <h1>{product.title}</h1>
+          <img
+            src={product.image}
+            alt={product.title}
+            style={{ maxWidth: "500px" }}
+          />
+          <p>Pris: {product.price} DKK</p>
+        </>
+      )}
+    </div>
+  );
 };
